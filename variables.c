e8eef737213042023641869f204cf9bc25ad63f6
George Calin <george.calin@gmail.com>
2020-03-22 18:27:03 +0200

    Experimenting with variables.

---
+++ variables.c
@@ -0,0 +1,29 @@
+/* *********************************************
+* Introducing variables and experimenting with variables 
+*
+************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int age;
+	age=30;
+	
+	printf("My age is %d years old",age);
+	
+	char x,y,z;
+	x='A';
+	y='B';
+	z='C';
+	
+	printf("\n It is just as simple as %c%c%c word",x,y,z);
+	
+    float i;
+	i=20.00/5.0;
+	
+	printf("\n The value of PI is %f" ,i);
+	
+	return (10);
+	
+}
