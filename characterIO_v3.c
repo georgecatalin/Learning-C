506c229a335d6ed206172aaea094be16d9c149e7
George Calin <george.calin@gmail.com>
2020-03-22 19:24:48 +0200

    Using character IO

---
+++ characterIO_v3.c
@@ -0,0 +1,26 @@
+/***************************************************
+* Character IO example 2
+****************************************************/
+
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	
+	printf("Type three characters in order: ");
+	
+	a=getchar();
+	putchar(a);
+	
+	b=getchar();
+	putchar(b);
+	
+	c=getchar();
+	putchar(c);
+	
+	return 10;
+	
+	
+}
